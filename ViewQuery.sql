bbf2b3b8f95ae15f210b70ed5f7c3f198daf32eb
prakashn27 <nprakashece@gmail.com>
2019-06-15 20:07:59 -0400

    updated view query

--- ViewQuery.sql
+++ ViewQuery.sql
@@ -1,2 +1,8 @@
 
+select * from UserManagement.dbo.FollowerTable;
+
 select * from UserManagement.dbo.UserTable;
+
+-- select EmailAddress 
+-- from UserManagement.dbo.FollowerTable
+-- where UserId = '092094-c72a-4aea-b900-cf3f971dc03d'
