8f5e8da73083b7fb959b6e698cb767fc105c3fb3
prakashn27 <nprakashece@gmail.com>
2019-06-17 19:22:15 -0400

    Parsed Contest and send to rmq

--- ParseContestApi/Controllers/ValuesController.cs
+++ ParseContestApi/Controllers/ValuesController.cs
@@ -5,6 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RabbitMQ.Client;
 using System.Text;
+using System.Linq;
+using ServiceStack;
+using ServiceStack.Text;
+using ParseContestApi.Models;
+// public class GithubRepository
+// {
+//     public string Name { get; set; }
+//     public string Description { get; set; }
+//     public string Url { get; set; }
+//     public string Homepage { get; set; }
+//     public string Language { get; set; }
+//     public int Watchers { get; set; }
+//     public int Forks { get; set; }
+
+//     public override string ToString() => Name;
+// }
+
+
+public class ChangedRanks {
+    public string Status {get; set;}
+    public ChangedRank[] Result { get; set;} 
+}
 
 namespace ParseContestApi.Controllers
 {
@@ -16,6 +38,23 @@ namespace ParseContestApi.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(string id)
         {
+            // var client = new RestClient("https://codeforces.com");
+            // var request = new RestRequest("api/contest.ratingChanges?contestId="+id);
+            // var response = client.Get(request);
+            // Console.WriteLine(response.Content);
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            var cfRanks = $"https://codeforces.com/api/contest.ratingChanges?contestId={id}"
+                .GetJsonFromUrl(httpReq => httpReq.UserAgent = "Gistlyn")
+                .FromJson<ChangedRanks>();
+                // .OrderByDescending(x => x.Watchers)
+                // .Take(5)
+                // .ToList();
+
+            // "Top 5 {0} Github Repositories:".Print(cfRanks);
+            // cfRanks.PrintDump();
+
+            "first rank {0} ".Print(cfRanks.Result[0]);
             var factory = new ConnectionFactory() { HostName = "localhost", UserName = "rabbitmquser", Password = "DEBmbwkSrzy9D1T9cJfa" };
             using (var connection = factory.CreateConnection())
             {
@@ -26,15 +65,20 @@ namespace ParseContestApi.Controllers
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
-
-                    string message = "Hello World!";
-                    var body = Encoding.UTF8.GetBytes(message);
-
-                    channel.BasicPublish(exchange: "Ranker",
+                    // var body = Encoding.UTF8.GetBytes(message);
+                    foreach(ChangedRank cr in cfRanks.Result) {
+                        var body = Encoding.UTF8.GetBytes(JsonSerializer.SerializeToString<ChangedRank>(cr));
+                        channel.BasicPublish(exchange: "Ranker",
                                          routingKey: "",
                                          basicProperties: null,
                                          body: body);
-                    Console.WriteLine(" [x] Sent {0}", message);
+                        Console.WriteLine(" [x] Sent {0}", cr.Handle);
+                    }
+                    // channel.BasicPublish(exchange: "Ranker",
+                    //                      routingKey: "",
+                    //                      basicProperties: null,
+                    //                      body: body);
+                    // Console.WriteLine(" [x] Sent {0}", message);
                 }
             }
             return id;
---
+++ ParseContestApi/Models/ChangedRank.cs
@@ -0,0 +1,13 @@
+namespace ParseContestApi.Models
+{
+    public class ChangedRank
+    {
+        public string ContestId { get; set; }
+        public string ContestName { get; set; }
+        public string Handle { get; set; }
+        public int Rank { get; set; }
+        public int OldRating { get; set; }
+        public int NewRating { get; set; }
+        public override string ToString() => Handle;
+    }
+}
