0b599c2fd780095736e4ba0eaebf444b0f939c89
prakashn27 <nprakashece@gmail.com>
2019-06-16 22:59:25 -0400

    added rmq publisher for parsecontestapi

---
+++ ParseContestApi/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ParseContestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class contestController : ControllerBase
+    {
+        // GET api/contest/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(string id)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "rabbitmquser", Password = "DEBmbwkSrzy9D1T9cJfa" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Rank",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "Ranker",
+                                         routingKey: "",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+            return id;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string id)
+        {
+
+        }
+    }
+}
