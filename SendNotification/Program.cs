3007eabe757515e69770610f2efd3b9dffa7f678
prakashn27 <nprakashece@gmail.com>
2019-06-19 08:33:38 -0400

    removed busy waiting, as it is not needed

--- SendNotification/Program.cs
+++ SendNotification/Program.cs
@@ -10,32 +10,32 @@ namespace SendNotification
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Send Notification service!");
-            while(true) {
-                var factory = new ConnectionFactory() { HostName = "localhost", UserName = "rabbitmquser", Password = "DEBmbwkSrzy9D1T9cJfa" };
-                using (var connection = factory.CreateConnection())
-                using (var channel = connection.CreateModel())
+            
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "rabbitmquser", Password = "DEBmbwkSrzy9D1T9cJfa" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Notification",
+                                        durable: true,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
                 {
-                    channel.QueueDeclare(queue: "Notification",
-                                         durable: true,
-                                         exclusive: false,
-                                         autoDelete: false,
-                                         arguments: null);
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "Notification",
+                                        autoAck: true,
+                                        consumer: consumer);
 
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
-                    {
-                        var body = ea.Body;
-                        var message = Encoding.UTF8.GetString(body);
-                        Console.WriteLine(" [x] Received {0}", message);
-                    };
-                    channel.BasicConsume(queue: "Notification",
-                                         autoAck: true,
-                                         consumer: consumer);
-
-                    Console.WriteLine(" Press [enter] to exit.");
-                    Console.ReadLine();
-                }
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
             }
+            
         }
     }
 }
