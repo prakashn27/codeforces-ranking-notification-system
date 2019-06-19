4f7ca0143a104b2be753e29e74159372b2efb214
prakashn27 <nprakashece@gmail.com>
2019-06-19 07:20:24 -0400

    basic working version of createnotification

--- CreateNotification/CreateNotificationService.cs
+++ CreateNotification/CreateNotificationService.cs
@@ -5,11 +5,24 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using ServiceStack;
+using ServiceStack.Text;
+using CreateNotification.Models;
 
 namespace CreateNotification
 {
     public class CreateNotificationService: IHostedService
     {
+
+        private readonly IServiceScopeFactory scopeFactory;
+
+        public CreateNotificationService(IServiceScopeFactory scopeFactory)
+        {
+            this.scopeFactory = scopeFactory;
+        }
         // CreateNotificationDBContext _dbContext;
         // public CreateNotificationService(CreateNotificationDBContext context) {
         //     _dbContext = context;
@@ -18,6 +31,11 @@ namespace CreateNotification
         public Task StartAsync(CancellationToken cancellationToken)
         {
             // Console.log("startAsync");
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<CreateNotificationDBContext>();
+        
+            }
             return Task.CompletedTask;
         }
 
--- CreateNotification/Program.cs
+++ CreateNotification/Program.cs
@@ -5,12 +5,16 @@ using System.Text;
 using ServiceStack;
 using ServiceStack.Text;
 using CreateNotification.Models;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using CreateNotification.DataAccess;
+using Microsoft.EntityFrameworkCore;
 
 namespace CreateNotification
 {
@@ -19,35 +23,7 @@ namespace CreateNotification
         static async Task Main(string[] args)
         {
             // Console.WriteLine("Starting Send Notification service!");
-            var host = new HostBuilder()
-                .ConfigureHostConfiguration(configHost =>
-                {
-                    configHost.SetBasePath(Directory.GetCurrentDirectory());
-                    configHost.AddJsonFile("hostsettings.json", optional: true);
-                    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
-                    configHost.AddCommandLine(args);
-                })
-                .ConfigureAppConfiguration((hostContext, configApp) =>
-                {
-                    configApp.AddJsonFile("appsettings.json", optional: true);
-                    configApp.AddJsonFile(
-                        $"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
-                        optional: true);
-                    configApp.AddEnvironmentVariables(prefix: "PREFIX_");
-                    configApp.AddCommandLine(args);
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<CreateNotificationService>();
-                })
-                .ConfigureLogging((hostContext, configLogging) =>
-                {
-                    configLogging.AddConsole();
-                    configLogging.AddDebug();
-                })
-                .UseConsoleLifetime()
-                .Build();
-
+            var host = CreateHostBuilder(args).Build();
             await host.RunAsync();
             // while (true)
             // {
@@ -77,6 +53,52 @@ namespace CreateNotification
             //         Console.ReadLine();
             //     }
             // }
+        }
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    configHost.AddJsonFile($"appsettings.json", optional: false);
+                    configHost.AddEnvironmentVariables();
+                    configHost.AddEnvironmentVariables("DOTNET_");
+                    configHost.AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: false);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // services.AddTransient<IMessageHandler>((svc) =>
+                    // {
+                    //     var rabbitMQConfigSection = hostContext.Configuration.GetSection("RabbitMQ");
+                    //     string rabbitMQHost = rabbitMQConfigSection["Host"];
+                    //     string rabbitMQUserName = rabbitMQConfigSection["UserName"];
+                    //     string rabbitMQPassword = rabbitMQConfigSection["Password"];
+                    //     // return new RabbitMQMessageHandler(rabbitMQHost, rabbitMQUserName, rabbitMQPassword, "Rankger", "Rank", ""); ;
+                    // });
+
+                    // services.AddTransient<AuditlogManagerConfig>((svc) =>
+                    // {
+                    //     var auditlogConfigSection = hostContext.Configuration.GetSection("Auditlog");
+                    //     string logPath = auditlogConfigSection["path"];
+                    //     return new AuditlogManagerConfig { LogPath = logPath };
+                    // });
+                    var sqlConnectionString = "server=localhost,1434;user id=sa;password=8jkGh47hnDw89Haq8LN2;database=UserManagement;";
+                    services.AddDbContext<CreateNotificationDBContext>(options => options.UseSqlServer(sqlConnectionString));
+
+                    services.AddHostedService<CreateNotificationService>();
+                })
+                // .UseSerilog((hostContext, loggerConfiguration) =>
+                // {
+                //     loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration);
+                // })
+                .UseConsoleLifetime();
+
+            return hostBuilder;
         }
     }
 }
